d41965ec1b6cbaeafecf09f6fa288b5edc6ef7bb
Gabrielly Lopes Pontes <gabriellylopesponntes@gmail.com>
2026-05-14 10:28:15 -0300

    feat(routes): add router to tipoSinal

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -5,13 +5,14 @@ const cuidadoresRouter = require('./cuidadorRoutes');
 const monitoradosRouter = require('./monitoradoRoutes');
 const contatosEmergenciaRouter = require('./contatoEmergenciaRoutes');
 const cuidadoresMonitoradosRouter = require('./cuidadorMonitoradoRoutes');
-const dispositivoRouter = require('./dispositivoRoutes');
+const dispositivosRouter = require('./dispositivoRoutes');
+const tiposSinaisRouter = require('./tipoSinalRoutes');
 
 router.use('/cuidadores', cuidadoresRouter);
 router.use('/monitorados', monitoradosRouter);  
 router.use('/contatos-emergencia', contatosEmergenciaRouter);
 router.use('/cuidadores-monitorados', cuidadoresMonitoradosRouter);
-router.use('/dispositivos', dispositivoRouter);
-
+router.use('/dispositivos', dispositivosRouter);
+router.use('/tipos-sinais', tiposSinaisRouter);
 
 module.exports = router;
